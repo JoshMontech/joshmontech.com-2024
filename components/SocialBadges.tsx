ced6e1f169f3bde80760a77f534d338b0d73ca53
Josh <JoshMontech@users.noreply.github.com>
2024-07-02 21:36:25 -0500

    updated linkedin link

--- components/SocialBadges.tsx
+++ components/SocialBadges.tsx
@@ -6,7 +6,7 @@ const SocialBadges = () => {
   return (
     <div className="flex gap-2 mt-1 items-center">
         <a href="https://github.com/JoshMontech"><FaGithub size={iconDimension} className="fill-white hover:fill-blue-light" /></a>
-        <a href="https://linkedin.com/joshmontech"><FaLinkedin size={iconDimension} className="fill-white hover:fill-blue-light" /></a>
+        <a href="https://linkedin.com/in/joshmontech"><FaLinkedin size={iconDimension} className="fill-white hover:fill-blue-light" /></a>
         <a href="https://codepen.io/jmontech"><FaCodepen size={iconDimension} className="fill-white hover:fill-blue-light" /></a>
         <a href="mailto:joshmontech@gmail.com"><FaEnvelope size={iconDimension} className="fill-white hover:fill-blue-light" /></a>
         <a href="tel:+19282558108"><FaPhone size={iconDimension - 3} className="fill-white hover:fill-blue-light" /></a>
