c14e22835df991e028661c59900cbcfa2b191f4d
Josh <JoshMontech@users.noreply.github.com>
2024-07-02 19:49:57 -0500

    added website SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,42 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "joshmontech.com - Josh Montgomery's Portfolio Website",
   description: "Senior Front End Developer Josh Montgomery's portfolio website. Demonstrates 8 years of experience and technical knowledge in React, NextJS, Tailwind, TypeScript, JavaScript, CSS3, HTML5, and More.",
+  keywords: ['Josh Montgomery', 'joshmontech', 'jmontech', 'front end', 'front-end', 'frontend', 'engineer', 'developer', 'senior'],
+  authors: [{ name: 'Josh Montgomery' }],
+  creator: 'Josh Montgomery',
+  publisher: 'Josh Montgomery',
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  openGraph: {
+    title: 'joshmontech.com - Josh Montgomery\'s Portfolio Website',
+    description: 'Senior Front End Developer Josh Montgomery\'s portfolio website. Demonstrates 8 years of experience and technical knowledge in React, NextJS, Tailwind, TypeScript, JavaScript, CSS3, HTML5, and More.',
+    url: 'https://www.joshmontech.com',
+    siteName: 'joshmontech.com',
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary',
+    title: 'joshmontech.com - Josh Montgomery\'s Portfolio Website',
+    description: 'Senior Front End Developer Josh Montgomery\'s portfolio website. Demonstrates 8 years of experience and technical knowledge in React, NextJS, Tailwind, TypeScript, JavaScript, CSS3, HTML5, and More.',
+    creator: '@jmontech',
+  },
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
 };
 
 export default function RootLayout({
